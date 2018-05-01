ff34807f21579ef584e4d382ec7af1a480ee0e82
arunk2 <emailarunkumar@gmail.com>
2018-05-01 16:14:56 +0530

    Added add user data to db.

--- main_dao.py
+++ main_dao.py
@@ -14,51 +14,83 @@ class MainDAO():
 	def create_database(self):
 		# Open database connection
 		db = MySQLdb.connect(self.host, self.user, self.password, self.database)
-		# prepare a cursor object using cursor() method
-		cursor = db.cursor()
 
-		# Drop table if it already exist using execute() method.
-		cursor.execute("DROP TABLE IF EXISTS ATHLETE")
+		try:
+			# prepare a cursor object using cursor() method
+			cursor = db.cursor()
 
-		# Create table as per requirement
-		sql = """CREATE TABLE ATHLETE (
-					id INT(11) NOT NULL,
-					firstname  VARCHAR(50),
-					lastname  VARCHAR(50),
-					sex CHAR(1),
-					email VARCHAR(50),
-					profile VARCHAR(200),
-					username VARCHAR(50),
-					access_token VARCHAR(100)
-					)"""
+			# Drop table if it already exist using execute() method.
+			cursor.execute("DROP TABLE IF EXISTS ATHLETE")
 
-		cursor.execute(sql)
-		cursor.execute("DROP TABLE IF EXISTS ACTIVITIES")
+			# Create table as per requirement
+			sql = """CREATE TABLE ATHLETE (
+						id INT(11) NOT NULL,
+						firstname  VARCHAR(50),
+						lastname  VARCHAR(50),
+						sex CHAR(1),
+						email VARCHAR(50),
+						profile VARCHAR(200),
+						username VARCHAR(50),
+						access_token VARCHAR(100)
+						)"""
 
-		sql = """CREATE TABLE ACTIVITIES (
-					id INT(11) NOT NULL,
-					athlete_id  INT(11),
-					athlete_firstname VARCHAR(50),
-					athlete_lastname  VARCHAR(50),
-					title  VARCHAR(50),
-					description  VARCHAR(100),
-					start_date datetime,
-					start_date_local datetime,
-					type VARCHAR(10),
-					distance  FLOAT,
-					distance_unit VARCHAR(5),
-					moving_time  FLOAT,
-					elapsed_time FLOAT,
-					)"""
+			cursor.execute(sql)
+			cursor.execute("DROP TABLE IF EXISTS ACTIVITIES")
 
-		cursor.execute(sql)
+			sql = """CREATE TABLE ACTIVITIES (
+						id INT(11) NOT NULL,
+						athlete_id  INT(11),
+						athlete_firstname VARCHAR(50),
+						athlete_lastname  VARCHAR(50),
+						title  VARCHAR(50),
+						description  VARCHAR(100),
+						start_date datetime,
+						start_date_local datetime,
+						type VARCHAR(10),
+						distance  FLOAT,
+						distance_unit VARCHAR(5),
+						moving_time  FLOAT,
+						elapsed_time FLOAT
+						)"""
+
+			cursor.execute(sql)
+			# Commit your changes in the database
+			db.commit()
+		except:
+		   # Rollback in case there is any error
+		   db.rollback()
 
 		# disconnect from server
 		db.close()
 
 
 	def add_athlete(user):
-		return None
+		# Open database connection
+		db = MySQLdb.connect(self.host, self.user, self.password, self.database)
+
+		try:
+			# prepare a cursor object using cursor() method
+			cursor = db.cursor()
+
+			sql = """INSERT INTO ATHLETE (id, firstname, lastname, sex, email, profile, username, access_token)
+							VALUES ({id}, '{firstname}', '{lastname}', '{sex}', '{email}', '{profile}', '{username}', '{access_token}')
+							)""".format(id=user['id'], firstname=user['firstname'],
+										lastname=user['lastname'], sex=user['sex'],
+										email=user['email'], profile=user['profile'],
+										username=user['username'], access_token=user['access_token'])
+			
+			print(sql)
+
+			cursor.execute(sql)
+			# Commit your changes in the database
+			db.commit()
+		except:
+		   # Rollback in case there is any error
+		   db.rollback()
+
+		# disconnect from server
+		db.close()
+		return True
 
 	def add_activity():
 		return None
