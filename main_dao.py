ea316e6b3e3265fc96626222e97cfc9fbb06f1ec
Arun Kumar Krishnan <emailarunkumar@gmail.com>
2023-03-08 23:25:43 -0800

    To fix refresh token and API updats

--- main_dao.py
+++ main_dao.py
@@ -24,21 +24,25 @@ class MainDAO():
 
 			# Create table as per requirement
 			sql = """CREATE TABLE ATHLETE (
-						id INT(11) NOT NULL PRIMARY KEY,
+						id BIGINT NOT NULL PRIMARY KEY,
 						firstname  VARCHAR(50),
 						lastname  VARCHAR(50),
 						sex CHAR(1),
 						email VARCHAR(50),
 						profile VARCHAR(200),
 						username VARCHAR(50),
-						access_token VARCHAR(100)
+						access_token VARCHAR(100),
+						refresh_token VARCHAR(100),
+						expires_at VARCHAR(100)
 						)"""
+
+			print(sql)
 
 			cursor.execute(sql)
 			cursor.execute("DROP TABLE IF EXISTS ACTIVITIES")
 
 			sql = """CREATE TABLE ACTIVITIES (
-						id INT(11) NOT NULL PRIMARY KEY,
+						id BIGINT NOT NULL PRIMARY KEY,
 						athlete_id  INT(11),
 						athlete_firstname VARCHAR(50),
 						athlete_lastname  VARCHAR(50),
@@ -47,11 +51,13 @@ class MainDAO():
 						start_date datetime,
 						start_date_local datetime,
 						type VARCHAR(10),
-						distance  FLOAT,
+						distance  VARCHAR(25),
 						distance_unit VARCHAR(5),
-						moving_time  FLOAT,
-						elapsed_time FLOAT
+						moving_time  VARCHAR(25),
+						elapsed_time VARCHAR(25)
 						)"""
+
+			print(sql)
 
 			cursor.execute(sql)
 			# Commit your changes in the database
@@ -72,14 +78,46 @@ class MainDAO():
 			# prepare a cursor object using cursor() method
 			cursor = db.cursor()
 
-			sql = """INSERT INTO ATHLETE (id, firstname, lastname, sex, email, profile, username, access_token)
-							VALUES ({id}, '{firstname}', '{lastname}', '{sex}', '{email}', '{profile}', '{username}', '{access_token}'
+			sql = """INSERT INTO ATHLETE (id, firstname, lastname, sex, email, profile, username, access_token, 
+			refresh_token, expires_at)
+							VALUES ({id}, '{firstname}', '{lastname}', '{sex}', '{email}', '{profile}', '{username}', 
+							'{access_token}', '{refresh_token}', '{expires_at}'
 							)""".format(id=user['id'], firstname=user['firstname'],
 										lastname=user['lastname'], sex=user['sex'],
 										email=user['email'], profile=user['profile'],
-										username=user['username'], access_token=user['access_token'])
+										username=user['username'], access_token=user['access_token'],
+										refresh_token=user['refresh_token'], expires_at=user['expires_at'])
+
+			print(sql)
+
+			cursor.execute(sql)
+			# Commit your changes in the database
+			db.commit()
+		except Exception as exception:
+			print(exception)
+			db.rollback()
+
+		# disconnect from server
+		db.close()
+		return True
 
 
+	def update_athlete(self, user):
+		# Open database connection
+		db = MySQLdb.connect(self.host, self.user, self.password, self.database)
+
+		try:
+			# prepare a cursor object using cursor() method
+			cursor = db.cursor()
+
+			sql = """UPDATE ATHLETE SET access_token = {access_token}, 
+										refresh_token = {refresh_token}, 
+										expires_at = {expires_at}
+										WHERE id = {id}
+							)""".format(id=user['id'], access_token=user['access_token'],
+										refresh_token=user['refresh_token'], expires_at=user['expires_at'])
+
+			print(sql)
 			cursor.execute(sql)
 			# Commit your changes in the database
 			db.commit()
@@ -105,7 +143,7 @@ class MainDAO():
 							VALUES ({id}, {athlete_id}, '{athlete_firstname}', '{athlete_lastname}',
 							'{title}', '{description}', '{start_date}', '{start_date_local}', '{type}',
 							'{distance}', '{distance_unit}', '{moving_time}',
-							'{elapsed_time}')""".format(id=activity['id'], athlete_id=activity.get('athlete_id',''),
+							'{elapsed_time}')""".format(id=activity['id'], athlete_id=activity.get('athlete_id', ''),
 										athlete_firstname=activity.get('athlete_firstname'), athlete_lastname=activity.get('athlete_lastname'),
 										title=activity.get('title'), description=activity.get('description'),
 										start_date=activity.get('start_date'), start_date_local=activity.get('start_date_local'),
@@ -135,7 +173,7 @@ class MainDAO():
 		cursor = db.cursor()
 
 		athletes = []
-		sql = "SELECT DISTINCT id, firstname, lastname, sex, email, profile, username, access_token FROM ATHLETE ORDER BY firstname"
+		sql = "SELECT DISTINCT id, firstname, lastname, sex, email, profile, username, access_token, refresh_token FROM ATHLETE ORDER BY firstname"
 		try:
 			# Execute the SQL command
 			cursor.execute(sql)
@@ -151,6 +189,7 @@ class MainDAO():
 				athlete['profile'] = row[5]
 				athlete['username'] = row[6]
 				athlete['access_token'] = row[7]
+				athlete['refresh_token'] = row[8]
 				athletes.append(athlete)
 
 		except Exception as exception:
@@ -191,7 +230,7 @@ class MainDAO():
 				activity['start_date'] = row[6].strftime("%Y-%B-%d")
 				activity['start_date_local'] = row[7].strftime("%Y-%B-%d")
 				activity['type'] = row[8]
-				activity['distance'] = round(float(row[9])/1000, 2)
+				activity['distance'] = row[9]
 				activity['distance_unit'] = row[10]
 				activity['moving_time'] = row[11]
 				activity['elapsed_time'] = row[12]
