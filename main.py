91c819e0d50ef31a92b756357d722b53c05aa04d
Arun Kumar Krishnan <emailarunkumar@gmail.com>
2023-03-08 23:25:07 -0800

    To fix refresh token and API updates

--- main.py
+++ main.py
@@ -25,13 +25,27 @@ def authorized_callback():
 	client = Client()
 	# Extract the code from your webapp response
 	code = request.args.get('code')
-	access_token = client.exchange_code_for_token(client_id=CONSTANTS.CLIENT_ID,
+	token_response = client.exchange_code_for_token(client_id=CONSTANTS.CLIENT_ID,
 												client_secret=CONSTANTS.CLIENT_SECRET,
 												code=code)
 	# Now store that access token along with athlete details
-	add_athlete(access_token)
-	#return jsonify({"status": "success"})
-	return redirect("http://34.211.61.161:3000/", code=302)
+	# add_athlete(access_token)
+	# Now store that access token along with athlete details
+	access_token = token_response['access_token']
+	refresh_token = token_response['refresh_token']
+	expires_at = token_response['expires_at']
+
+	client.access_token = access_token
+	athlete = client.get_athlete()
+	ath = extract_athlete(athlete)
+	ath['access_token'] = access_token
+	ath['refresh_token'] = refresh_token
+	ath['expires_at'] = expires_at
+	dao = MainDAO()
+	dao.add_athlete(ath)
+	pull_activities(access_token, refresh_token, ath['id'], ath['firstname'], ath['lastname'])
+
+	return redirect(CONSTANTS.UI_HOMEPAGE_URL, code=302)
 
 
 def get_athletes():
@@ -63,13 +77,14 @@ def dump_activities():
 	print(from_date)
 	for athlete in result:
 		access_token = athlete['access_token']
-		pull_activities(access_token, athlete['firstname'], athlete['lastname'], from_date)
+		refresh_token = athlete['refresh_token']
+		pull_activities(access_token, refresh_token, athlete['id'], athlete['firstname'], athlete['lastname'], from_date)
 	return jsonify({"status": "success"})
 
 
 def create_db():
 	dao = MainDAO()
-	dao.create_db()
+	dao.create_database()
 	return jsonify({"status": "success"})
 
 
@@ -104,10 +119,23 @@ def extract_athlete(athlete):
 	return ath
 
 
-def pull_activities(access_token, firstname, lastname, from_time = "2018-01-01T00:00:00Z"):
+def pull_activities(access_token, refresh_token, id, firstname, lastname, from_time = "2022-01-01T00:00:00Z"):
 	# An authorized callback is coming. Process it and add
 	client = Client()
-	client.access_token = access_token
+	refresh_response = client.refresh_access_token(client_id=CONSTANTS.CLIENT_ID, client_secret=CONSTANTS.CLIENT_SECRET,
+												   refresh_token = refresh_token)
+
+	ath = {}
+	ath['access_token'] = refresh_response['access_token']
+	ath['refresh_token'] = refresh_response['refresh_token']
+	ath['expires_at'] = refresh_response['expires_at']
+	ath['id'] = id
+	dao = MainDAO()
+	dao.update_athlete(ath)
+
+	client.access_token = refresh_response['access_token']
+	client.refresh_token = refresh_response['refresh_token']
+
 	for activity in client.get_activities(after = from_time,  limit=500):
 		process_activity(activity, firstname, lastname)
 
@@ -132,22 +160,13 @@ def extract_activity(activity):
 		act['title'] = str(activity.name.encode('ascii', 'ignore')).replace("'", "")
 		act['description'] = activity.description
 		act['type'] = activity.type
-		act['distance'] = activity.distance.get_num()
-		act['distance_unit'] = activity.distance.get_unit().get_specifier()
-		act['moving_time'] = round(activity.moving_time.total_seconds()/60.0)
-		act['elapsed_time'] = round(activity.elapsed_time.total_seconds()/60.0)
+		act['distance'] = activity.distance
+		act['distance_unit'] = "m"
+		act['moving_time'] = activity.moving_time
+		act['elapsed_time'] = activity.elapsed_time
 		act['start_date'] = activity.start_date
 		act['start_date_local'] = activity.start_date_local
 
-		# act['location_city'] = activity.location_city
-		# act['location_country'] = activity.location_country
-		# act['location_state'] = activity.location_state
-		# act['start_date'] = activity.start_date
-		# act['start_date_local'] = activity.start_date_local
-		# act['start_latitude'] = activity.start_latitude
-		# act['start_latlng'] = activity.start_latlng
-		# act['start_longitude'] = activity.start_longitude
-		# act['workout_type'] = activity.workout_type
 	except Exception as excep:
 		print(excep)
 	return act
