c714d38b4c9270b353d39835b9f331ba17124570
Arun Kumar Krishnan <emailarunkumar@gmail.com>
2023-03-08 23:26:26 -0800

    Updated endpoint

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -1,8 +1,10 @@
 DB_HOST = 'localhost'
-DB_USER = 'root'
+DB_USER = 'ttb'
 DB_PASS = 'ttb'
 DB_DATABASE= 'ttb'
 
-CALLBACK_URL	= 'http://csika.org:8082/authorized'
+CALLBACK_URL	= 'http://ec2-3-218-241-2.compute-1.amazonaws.com:8082/authorized'
 CLIENT_ID		= 25184
 CLIENT_SECRET	= ''
+
+UI_HOMEPAGE_URL = 'http://ec2-3-218-241-2.compute-1.amazonaws.com/'
